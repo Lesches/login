3ba93341007b5fe2d9a38d23f8d286788b17dded
sholom <sholomls@gmail.com>
2019-06-12 13:24:44 -0400

    get words

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+let rando = require('random-words');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/word', (req, res) =>{
+  let word = rando(1);
+  res.send({word: word});
+    }
+)
+
+router.get('/words/:count', (req, res) =>{
+  let count = Number(req.params.count);
+
+      let word = rando({exactly: count, join: ' '});
+      res.send({words: word});
+    }
+)
+
+router.post('/word', (req, res) =>{
+  let word = rando(1);
+      res.send({word: 'Trump'});
+    }
+)
 
 module.exports = router;
