4ad31d5f7617e71f1953f101e51ad51cb4532487
sholom <sholomls@gmail.com>
2019-06-12 13:43:49 -0400

    get sentence

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,22 @@ router.get('/word', (req, res) =>{
   let word = rando(1);
   res.send({word: word});
     }
-)
+);
 
 router.get('/words/:count', (req, res) =>{
   let count = Number(req.params.count);
 
-      let word = rando({exactly: count, join: ' '});
-      res.send({words: word});
+      let words = rando({exactly: count, join: ' '});
+      res.send({words: words});
     }
-)
+);
+
+router.get('/sentence', (req, res) => {
+  min = (isNaN(Number(req.query.min)))? 1: Number(req.query.min);
+  max = (isNaN(Number(req.query.max)))? min +1: Number(req.query.max);
+  let sentence = rando({min:min, max:max, join: ' '});
+  res.send({sentence:sentence});
+});
 
 router.post('/word', (req, res) =>{
   let word = rando(1);
